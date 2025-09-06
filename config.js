7030679755ba1bc53b570bc5bfcbf793d86f9887
prapti <praptishah2823@gmail.com>
2025-09-06 09:45:22 +0530

    changes updated in config file

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const data = mongoose.connect("mongodb://0.0.0.0:27017/contact").then(()=>{
+const data = mongoose.connect("mongodb+srv://praptishah2823:praptishah2823_U!@portfoliocontact.wp0gsg0.mongodb.net/").then(() => {
     console.log("Connected");
-}).catch((error)=>{
-    console.log('Error in connecting the database: ',error)
+}).catch((error) => {
+    console.log('Error in connecting the database: ', error)
 })
 
 module.exports = data;

f68082aebb8718bdb0a4a55b0bbdf56c356a7a5c
prapti <praptishah2823@gmail.com>
2025-09-09 21:49:48 +0530

    line to be comment

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 const data = mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true
 }).then(() => {
     console.log("Connected");
 }).catch((error) => {
     console.log('Error in connecting the database: ', error)
 })
-// }"mongodb+srv://praptishah2823:praptishah2823_U!@portfoliocontact.wp0gsg0.mongodb.net/",{
+// }"mongodb+srv://praptishah2823:praptishah2823_U!@PortfolioContact.wp0gsg0.mongodb.net/PortfolioDB?retryWrites=true&w=majority",{
 // useNewUrlParser: true,
 // useUnifiedTopology: true,
 // }

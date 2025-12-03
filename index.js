eadfc07cfcd49b422e88988d0a99d7e74cd1c7f7
prapti <praptishah2823@gmail.com>
2025-12-03 18:41:03 +0530

    index file updation

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use(route);
+app.use('/', route);
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });

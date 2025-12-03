aeba16046f383a7517505f4a3bbdfe7bfe9aae35
prapti <praptishah2823@gmail.com>
2025-12-03 18:55:35 +0530

    testing index file

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Test route
+app.get('/test', (req, res) => {
+    res.json({ message: 'Server is working!' });
+});
+
 app.use('/', route);
 
 app.listen(PORT, '0.0.0.0', () => {

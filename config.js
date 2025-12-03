b2027c66d4530e4b379a08e37a8e17ec7b687007
prapti <praptishah2823@gmail.com>
2025-12-03 18:35:28 +0530

    config file updation

--- config.js
+++ config.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
-console.log('Environment variable MONDODB_CONNECTION:', process.env.MONDODB_CONNECTION);
-
 const connectionString = process.env.MONDODB_CONNECTION || "mongodb+srv://praptishah2823:praptishah2823_U!@portfoliocontact.wp0gsg0.mongodb.net/PortfolioDB";
-
-console.log('Using connection string:', connectionString);
 
 const data = mongoose.connect(connectionString).then(() => {
     console.log("Database connected successfully");

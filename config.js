b3055357cae05935430dc011d7379fc6dd351beb
prapti <praptishah2823@gmail.com>
2025-12-03 18:01:59 +0530

    Configure for Render deployment - remove Netlify config, add env variables

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-const data = mongoose.connect("mongodb+srv://praptishah2823:praptishah2823_U!@portfoliocontact.wp0gsg0.mongodb.net/",{
+const data = mongoose.connect(process.env.MONDODB_CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
-    console.log("Connected");
+    console.log("Database connected successfully");
 }).catch((error) => {
-    console.log('Error in connecting the database: ', error)
-})
+    console.log('Error in connecting the database: ', error);
+});
 
 module.exports = data;
--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 const express = require('express');
-const serverless= require('serverless-http');
 const app = express();
 const cors = require('cors');
-const route= require('./Routers/routes');
+const route = require('./Routers/routes');
 require('./config');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 app.use(route);
 
-app.use('./netlify/functions/api',route)
-
-// app.listen(PORT,()=>{
-//     console.log(`Server is connected on port ${PORT}`)
-// })
-
-module.exports.handler = serverless(app)
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});

75c1e25df8e54f5671df311c747448e5aa1a20d2
prapti <praptishah2823@gmail.com>
2025-12-03 18:31:57 +0530

    config file updated

--- config.js
+++ config.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectionString = process.env.MONDODB_CONNECTION || "mongodb+srv://praptishah2823:praptishah2823_U!@portfoliocontact.wp0gsg0.mongodb.net/";
 
-const data = mongoose.connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
+const data = mongoose.connect(connectionString).then(() => {
     console.log("Database connected successfully");
 }).catch((error) => {
     console.log('Error in connecting the database: ', error);

6228f8f6ba5cfcb695cd86b8c67892b21c96dbbd
prapti <praptishah2823@gmail.com>
2025-12-03 18:12:59 +0530

    changes done for mongo db database

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const data = mongoose.connect(process.env.MONDODB_CONNECTION, {
+const connectionString = process.env.MONDODB_CONNECTION || "mongodb+srv://praptishah2823:praptishah2823_U!@portfoliocontact.wp0gsg0.mongodb.net/";
+
+const data = mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
